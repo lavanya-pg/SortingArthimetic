1cab5d5161c06850d799cb7cd96120fcf99e6dce
lavanya-pg <lavanyapg8@gmail.com>
2021-06-13 13:40:41 +0530

    getting o/p for res1

--- Sortingarthimetic.sh
+++ Sortingarthimetic.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -x
 echo "welcome to sorting arthimetic"
 read -p "enter value for a:" a
 read -p "enter value for b:" b
 read -p "enter value for c:" c
+res1=$(($a+$b*$c))
+echo $res1
 
 
